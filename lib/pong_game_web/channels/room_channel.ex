565bb0cc7f19c91be78e1c5d4566852d7fc93765
Marley Mokomoko-Young <marley@prefer.co.nz>
2019-03-01 10:24:18 +1300

    - minor optimization

--- lib/pong_game_web/channels/room_channel.ex
+++ lib/pong_game_web/channels/room_channel.ex
@@ -6,9 +6,18 @@ defmodule PongGameWeb.RoomChannel do
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
-    pid = GenServer.whereis({:global, :default_game})
-    game_state = GenServer.call(pid, :increment_count)
+    game_state = GenServer.call({:global, :default_game}, :increment_count)
     broadcast!(socket, "new_msg", game_state)
+    {:noreply, socket}
+  end
+
+  def handle_in("start_timer", _, socket) do
+    PongGameWeb.Endpoint.broadcast("timer:start", "start_timer", %{})
+    {:noreply, socket}
+  end
+
+  def handle_in("new_time", msg, socket) do
+    push socket, "new_time", msg
     {:noreply, socket}
   end
 end
