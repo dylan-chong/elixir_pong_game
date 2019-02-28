2ca777a4dc9d7ad133268270dfda691b3174004b
Marley Mokomoko-Young <marley@prefer.co.nz>
2019-03-01 10:26:35 +1300

    - remove unnecessary code

--- lib/pong_game_web/channels/room_channel.ex
+++ lib/pong_game_web/channels/room_channel.ex
@@ -10,14 +10,4 @@ defmodule PongGameWeb.RoomChannel do
     broadcast!(socket, "new_msg", game_state)
     {:noreply, socket}
   end
-
-  def handle_in("start_timer", _, socket) do
-    PongGameWeb.Endpoint.broadcast("timer:start", "start_timer", %{})
-    {:noreply, socket}
-  end
-
-  def handle_in("new_time", msg, socket) do
-    push socket, "new_time", msg
-    {:noreply, socket}
-  end
 end
